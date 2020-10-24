2416e11d6e585bd9a3078b3bec53ecdb4a35446f
Aniko Magyar <mofolade@gmail.com>
2020-10-24 22:33:26 +0200

    added authentiocationEntryPoint

--- src/main/java/com/example/pokemon/config/AuthenticationEntryPoint.java
+++ src/main/java/com/example/pokemon/config/AuthenticationEntryPoint.java
@@ -1,23 +1,29 @@
 package com.example.pokemon.config;
 
 
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
-@Component
-public class AuthenticationEntryPoint /*extends BasicAuthenticationEntryPoint*/ {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
-    /*@Override
+@Component
+public class AuthenticationEntryPoint extends BasicAuthenticationEntryPoint {
+
+    @Override
     public void commence(HttpServletRequest request, HttpServletResponse response,
                          AuthenticationException authException) throws IOException {
         response.addHeader("WWW-Authenticate", "Basic realm - " + getRealmName());
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED); // 401
         var writer = response.getWriter();
         writer.println("Http Status 401: " + authException.getMessage());
-    }*/
-/*
+    }
+
     @Override
     public void afterPropertiesSet() {
         setRealmName("SupernovaIT");
         super.afterPropertiesSet();
-    }*/
+    }
 }
--- src/main/java/com/example/pokemon/services/MyUserDetailsService.java
+++ src/main/java/com/example/pokemon/services/MyUserDetailsService.java
@@ -5,18 +5,21 @@ import com.example.pokemon.entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
 import java.util.stream.Collectors;
 
 @Service
-public class MyUserDetailsService {
+public class MyUserDetailsService implements UserDetailsService {
 
     @Autowired
     private UserService userService;
 
-   /* @Override
+    @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userService.findByUsername(username);
         if(user == null) {
@@ -24,7 +27,7 @@ public class MyUserDetailsService {
         }
         return new org.springframework.security.core.userdetails.User(user.getUsername(),
                 user.getPassword(), getGrantedAuthorities(user));
-    }*/
+    }
 
     private Collection<GrantedAuthority> getGrantedAuthorities(User user) {
         return user.getRoles().stream()
